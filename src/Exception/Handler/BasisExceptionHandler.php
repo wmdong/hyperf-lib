ae04ea3fcc188903e20e53417c73163e5c940a5a
wmud <wmud@gmail>
2023-11-12 17:30:29 +0800

    fix 异常处理响应

--- src/Exception/Handler/BasisExceptionHandler.php
+++ src/Exception/Handler/BasisExceptionHandler.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Wmud\HyperfLib\Exception\Handler;
 
+use Hyperf\Context\ApplicationContext;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Wmud\HyperfLib\Log\AppLog;
 use Hyperf\Collection\Arr;
-use Hyperf\Di\Annotation\Inject;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
@@ -16,21 +18,11 @@ use Throwable;
 class BasisExceptionHandler extends ExceptionHandler
 {
     /**
-     * @var RequestInterface
-     */
-    #[Inject]
-    protected RequestInterface $request;
-
-    /**
-     * @var ResponseInterface
-     */
-    #[Inject]
-    protected ResponseInterface $response;
-
-    /**
      * @param Throwable $throwable
      * @param PsrResponseInterface $response
      * @return PsrResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      */
     public function handle(Throwable $throwable, PsrResponseInterface $response): PsrResponseInterface
     {
@@ -54,17 +46,21 @@ class BasisExceptionHandler extends ExceptionHandler
      * @param array $context 日志内容
      * @param int $status http状态
      * @return PsrResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      */
     protected function responseHandle(array $data, string $message, string $level, array $context = [], int $status = 200): PsrResponseInterface
     {
-        // 日志信息
-        $params = $this->request->all();
-        $serverParams = $this->request->getServerParams();
+        $container = ApplicationContext::getContainer();
+        $request = $container->get(RequestInterface::class);
+        $response = $container->get(ResponseInterface::class);
+        $params = $request->all();
+        $serverParams = $request->getServerParams();
         $ip = Arr::get($serverParams, 'remote_addr');
         $uri = Arr::get($serverParams, 'request_uri');
         $context['params'] = $params;
         $context['response'] = $data;
         AppLog::$level("[IP: $ip] [URI: $uri] [Message: $message]", $context);
-        return $this->response->json($data)->withStatus($status);
+        return $response->json($data)->withStatus($status);
     }
 }
